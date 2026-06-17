105f069f1d56a1890e80c39167565bd7abe98651
amkjitu <amkhanshadhin@gmail.com>
2026-06-17 18:53:15 +0600

    day-7: 12hr:44min

---
+++ CJ_18_Demo.java
@@ -0,0 +1,23 @@
+/*
+ * Optional Class in Java is a container object which may or may not contain a non-null value. It is a part of the java.util package and was introduced in Java 8 to provide a more functional approach to handling null values and to avoid NullPointerExceptions. Optional provides methods to check if a value is present, to retrieve the value if it is present, and to perform actions based on the presence or absence of a value. It is often used to represent the result of a method that may return a value or may return null, allowing developers to write more robust and readable code by explicitly handling the possibility of null values.
+*/
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+public class CJ_18_Demo {
+    public static void main(String[] args) {
+        List<String> namesString = Arrays.asList("Alice", "Bob", "Charlie", "David", "Eve");
+        // 1. Create an Optional from the first name that starts with "A"
+        Optional<String> names = namesString.stream()
+                .filter(name -> name.startsWith("Z"))
+                .findFirst();
+
+        // 2. Check if the Optional contains a value and print it
+        System.out.println("Optional contains value: " + names.get());
+        // this will throw NoSuchElementException because there is no name that starts with "Z" in the list, so the Optional is empty and does not contain a value.
+        
+
+    }
+}
